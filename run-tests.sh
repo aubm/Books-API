c8e787acaa900a45563dc640e3f46ad62e6c8e02
kendo5731 <aur.baumann@gmail.com>
2016-03-28 20:06:22 +0200

    chore(): configures integration with Travis CI

---
+++ run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker build -t aubm/books-api .
+docker-compose up -d --force-recreate
+docker-compose ps
+echo "waiting for a few seconds for the app to be ready..."
+sleep 15
+go test ./...
+newman -c postman/collection.json -e postman/environnement.json -d postman/data.json -x
